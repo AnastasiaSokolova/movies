109dbd0aaa9339251f3ee3d33ef81b0b8559caf1
KYIV\Anastasiia_Sokolova <Anastasiia_Sokolova@epam.com>
2018-10-16 22:24:58 +0300

    updates

--- src/app/components/callbackPage.jsx
+++ src/app/components/callbackPage.jsx
@@ -1,1 +1,39 @@
-git fetch
+import React, {Component} from "react";
+import { connect } from "react-redux";
+import {bindActionCreators} from 'redux';
+import {setUser} from '../store/actions';
+import { CallbackComponent } from "redux-oidc";
+import { push } from "react-router-redux";
+import userManager from "../utils/userManager.jsx";
+
+import { withRouter } from 'react-router-dom'
+
+class CallbackPage extends Component {
+
+
+    render() {
+        return (
+            <CallbackComponent
+                userManager={userManager}
+                successCallback={(user) => {
+                   this.props.dispatch(push("localhost:8080/"));
+                  }
+                }
+                errorCallback={error => {
+                      this.props.dispatch(push("/"));
+                      console.error(error);
+                }}
+            >
+                <div>Redirecting...</div>
+            </CallbackComponent>
+        );
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    dispatch: dispatch,
+    setUser: bindActionCreators((data) => setUser(data), dispatch),
+});
+
+
+export default withRouter(connect(null, mapDispatchToProps)(CallbackPage));
